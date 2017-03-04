f614293df3b6d9454f87c8775a38982745d4144a
Soreine <soreine.plume@gmail.com>
2017-03-04 01:17:36 +0100

    Fix deploy script...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,9 @@ IFS=$'\n\t'
 elm-make main.elm --output main.js
 
 # Deploy
+cp index.html style.css main.js dist
+
+git add -f dist/
+git ci -m 'Deploy gh-pages'
+
 git subtree push --prefix dist origin gh-pages
